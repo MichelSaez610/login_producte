bc6d1f92cc4e9bc63cb4dcdf50372bcc34904b7e
Michelsaez610 <michel.saez@institutvidreres.cat>
2024-10-20 18:38:30 +0200

    Fixed Login and started product

--- app/src/main/java/com/login/login/ProducteActivity.kt
+++ app/src/main/java/com/login/login/ProducteActivity.kt
@@ -1,6 +1,8 @@
 package com.login.login
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,12 @@ class ProducteActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val dropdown: Spinner = findViewById(R.id.dropdown_menu)
+
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.dropdown_items, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        dropdown.setAdapter(adapter)
     }
 }
